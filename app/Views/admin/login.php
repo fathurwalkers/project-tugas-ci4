df0b8f7752f2b2d439917a6a2eab1e95988124e1
Fathur Walkers <fathurwalkers44@gmail.com>
2021-02-10 03:43:15 -0800

    skdoksoakaodas

--- app/Views/admin/login.php
+++ app/Views/admin/login.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>DARKMODEMOBILE</title>
+  <title>BEST FRIEND SQUAD</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet"
@@ -25,10 +25,10 @@
     <!-- /.login-logo -->
     <div class="card card-outline card-primary">
       <div class="card-header text-center">
-        <a href="../../index2.html" class="h1">DARKMODEMOBILE</a>
+        <a href="../../index2.html" class="h1">BEST FRIEND SQUAD</a>
       </div>
       <div class="card-body">
-        <p class="login-box-msg">LOGIN KE DARKMODEMOBILE</p>
+        <p class="login-box-msg">LOGIN KE BEST FRIEND SQUAD</p>
 
         <form
           action="<?= base_url('login/post') ?>"
--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>DARK MODE MOBILE</title>
+  <title>BEST FRIEND SQUAD</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet"
@@ -49,14 +49,14 @@
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar bg-primary elevation-4 position-fixed">
+    <aside class="main-sidebar bg-info elevation-4 position-fixed">
       <!-- Brand Logo -->
       <a href="<?php echo base_url('/'); ?>/adminlte/index3.html"
         class="brand-link">
         <img
           src="<?php echo base_url('/'); ?>/adminlte/dist/img/AdminLTELogo.png"
           alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">M - DARK MODE</span>
+        <span class="brand-text font-weight-light">BESTFRIENDSQUAD</span>
       </a>
 
       <!-- Sidebar -->
